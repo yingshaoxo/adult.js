ff246388afabbc833ff29908ec79455b040b3671
yingshaoxo <yingshaoxo@gmail.com>
2020-07-18 05:17:47 +0800

    I think I just finished this project

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # adult.js
-~~Hey, child! You are not allowed here!~~
-
-Hey, kids! You shouldn't be allowed to be here!
+Hey, kids! You are not allowed in here!
--- src/index.tsx
+++ src/index.tsx
@@ -1,36 +1,66 @@
 import * as React from 'react'
 import styles from './styles.module.css'
 import * as faceapi from 'face-api.js';
-import Webcam from 'react-webcam';
+import { Dialog, Paragraph } from 'evergreen-ui'
 
-
-
-interface WebcamProps {
-  webcamRef: React.RefObject<Webcam & HTMLVideoElement>
-  handle_video_element: () => void
+interface MirrorProps {
+  handle_video_element: (video_element: HTMLVideoElement, canvas_element: HTMLCanvasElement) => void
 }
+interface MirrorState {
+};
 
-class WebcamCapture extends React.Component<WebcamProps> {
-  componentDidMount() {
-    this.props.handle_video_element()
+class Mirror extends React.Component<MirrorProps, MirrorState> {
+  video_reference: HTMLVideoElement | null = null
+  canvas_reference: HTMLCanvasElement
+  set_video_reference = (element: HTMLVideoElement) => {
+    this.video_reference = element
   }
-
+  set_canvas_reference = (element: HTMLCanvasElement) => {
+    this.canvas_reference = element
+  }
+  async componentDidMount() {
+    if (this.video_reference) {
+      let video_stream: MediaStream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: "user" }, audio: false })
+      this.video_reference.srcObject = video_stream
+      this.props.handle_video_element(this.video_reference, this.canvas_reference)
+    }
+  }
   render() {
-    const videoConstraints = {
-      width: 1280,
-      height: 720,
-      facingMode: "user"
-    };
-
     return (
-      <Webcam
-        ref={this.props.webcamRef}
-        videoConstraints={videoConstraints}
-      />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <video
+          ref={this.set_video_reference}
+          id="player"
+          autoPlay
+          width="400"
+          height="400"
+          style={{
+            position: "absolute"
+          }}
+        >
+        </video>
+        <canvas
+          ref={this.set_canvas_reference}
+          id="canvas"
+          style={{
+            position: "absolute",
+          }}
+        >
+        </canvas>
+      </div>
     )
   }
 }
 
+interface DisplaySize {
+  height: number
+  width: number
+}
 interface Props {
   weights_path: string
   show: boolean
@@ -39,7 +69,10 @@ interface Props {
 interface State {
   weights_path: string
   show: boolean
-  webcamRef: React.RefObject<Webcam & HTMLVideoElement>
+  agree: boolean
+  asked: boolean
+  check_counting: number
+  check_age_array: Array<number>
 }
 
 class ChildGuard extends React.Component<Props, State> {
@@ -49,49 +82,126 @@ class ChildGuard extends React.Component<Props, State> {
     this.state = {
       weights_path: props.weights_path,
       show: props.show,
-      webcamRef: React.createRef<Webcam & HTMLVideoElement>(),
+      agree: false,
+      asked: false,
+      check_counting: 0,
+      check_age_array: [],
     };
-    this.handle_video_element = this.handle_video_element.bind(this)
+    this.handle_video_element = this.handle_video_element.bind(this);
     this.detection = this.detection.bind(this)
   }
 
-  async detection() {
-      const options = new faceapi.TinyFaceDetectorOptions({ inputSize: 320 })
-      const blob = this.state.webcamRef?.current?.getScreenshot()
-      //console.log(blob)
-      if (blob) {
-        const img = await faceapi.fetchImage(blob);
-        const detections = await faceapi.detectSingleFace(img, options).withAgeAndGender()
-        console.log(detections)
+  async detection(video_element: HTMLVideoElement, canvas_element: HTMLCanvasElement, displaySize: DisplaySize) {
+    const detections = await faceapi.detectSingleFace(video_element).withAgeAndGender()
+    if (detections) {
+      const resizedDetections = faceapi.resizeResults(detections, displaySize)
+      const age = Math.floor(resizedDetections.age)
+      this.setState({
+        check_age_array: [...this.state.check_age_array, age]
+      })
+      const drawOptions = {
+        label: age.toString(),
+        lineWidth: 2,
+        boxColor: "#FF3D00",
       }
+      const drawBox = new faceapi.draw.DrawBox(resizedDetections.detection.box, drawOptions)
+      drawBox.draw(canvas_element)
+    }
 
+    if (this.state.check_counting < 3) {
       setTimeout(async () => {
-        this.detection()
+        var ctx = canvas_element.getContext("2d");
+        ctx?.clearRect(0, 0, canvas_element.width, canvas_element.height);
+        await this.detection(video_element, canvas_element, displaySize)
       }, 1500)
+      this.setState({
+        check_counting: this.state.check_counting + 1
+      })
+    } else {
+      const getMean = (array: Array<number>) => array.reduce((a, b) => a + b) / array.length;
+      const mean = getMean(this.state.check_age_array)
+      const adult = mean > 18 ? true : false
+      this.props.callback_function(adult, Math.floor(mean))
+      this.setState({
+        show: false
+      })
+    }
   }
 
-  async handle_video_element() {
-    await faceapi.nets.tinyFaceDetector.loadFromUri(this.state.weights_path)
+  async handle_video_element(video_element: HTMLVideoElement, canvas_element: HTMLCanvasElement) {
+    await faceapi.nets.ssdMobilenetv1.loadFromUri(this.state.weights_path)
     await faceapi.nets.ageGenderNet.loadFromUri(this.state.weights_path)
     console.log("faceapi loaded.")
 
-    this.detection()
+    // for resizing the overlay canvas to the video dimensions
+    const displaySize = { width: video_element.width, height: video_element.height }
+    faceapi.matchDimensions(canvas_element, displaySize)
+
+    await this.detection(video_element, canvas_element, displaySize)
+  }
+
+  canceled = () => {
+    this.setState({
+      asked: true,
+      agree: false,
+    })
+    console.log("canceled")
+
+    this.props.callback_function(false, 0)
+    this.setState({
+      show: false
+    })
   }
 
   render() {
-    return (
-      this.state.show ?
+    if (this.state.asked == false) {
+      return (
+        <Dialog
+          isShown={!this.state.asked}
+          intent="danger"
+          title="Attention"
+          confirmLabel="Let's do it!"
+          onCloseComplete={() => {
+            this.canceled()
+          }}
+          onCancel={() => {
+            this.canceled()
+          }}
+          onConfirm={() => {
+            this.setState({
+              asked: true,
+              agree: true,
+            })
+            console.log("agreed")
+          }}
+        >
+          <Paragraph>
+            We will need to use your camera to do a check to see if you are an adult.
+          </Paragraph>
+          <Paragraph>
+            We won't collect your data, because it's all happened on your browser.
+          </Paragraph>
+          <Paragraph>
+            Would you willing to go on with this?
+          </Paragraph>
+        </Dialog>
+      )
+    }
+
+    if (this.state.show && this.state.agree) {
+      return (
         <div
           className={styles.test}
         >
-          <WebcamCapture
-            webcamRef={this.state.webcamRef}
+          <Mirror
             handle_video_element={this.handle_video_element}
-          ></WebcamCapture>
+          ></Mirror>
         </div>
-        :
-        null
-    )
+      )
+    }
+    else {
+      return null
+    }
   }
 }
 
